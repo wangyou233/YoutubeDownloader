4bf33f23c7ab2bf132686ace48fafd047428f6f9
XS <37208063+qq790382720@users.noreply.github.com>
2024-01-08 09:34:56 +0800

    Update MediaFile.cs

--- YoutubeDownloader.Core/Tagging/MediaFile.cs
+++ YoutubeDownloader.Core/Tagging/MediaFile.cs
@@ -8,19 +8,19 @@ internal partial class MediaFile(TagFile file) : IDisposable
 {
     public void SetThumbnail(byte[] thumbnailData) =>
         file.Tag.Pictures = new IPicture[] { new Picture(thumbnailData) };
-
+    ///设置Artist
     public void SetArtist(string artist) => file.Tag.Performers = new[] { artist };
-
-
+    
+    ///设置ArtistSort
     public void SetArtistSort(string artistSort) => file.Tag.PerformersSort = new[] { artistSort };
 
     ///设置Title
     public void SetTitle(string title) => file.Tag.Title = title;
-
+    ///设置Album
     public void SetAlbum(string album) => file.Tag.Album = album;
-
+    ///设置Description
     public void SetDescription(string description) => file.Tag.Description = description;
-
+    ///设置Comment
     public void SetComment(string comment) => file.Tag.Comment = comment;
 
     public void Dispose()
