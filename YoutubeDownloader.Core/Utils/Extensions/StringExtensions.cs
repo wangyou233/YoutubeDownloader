d8743cd2c3e8f8ca5cbe962a91044f8f8d6d9bb5
cz0004 <37764297+cz0004@users.noreply.github.com>
2024-01-09 09:21:15 +0800

    add StringExtensions

--- YoutubeDownloader.Core/Utils/Extensions/StringExtensions.cs
+++ YoutubeDownloader.Core/Utils/Extensions/StringExtensions.cs
@@ -4,4 +4,22 @@ public static class StringExtensions
 {
     public static string? NullIfEmptyOrWhiteSpace(this string str) =>
         !string.IsNullOrEmpty(str.Trim()) ? str : null;
+    /// <summary>
+    /// 判断字符串是否为Null、空
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static bool IsNull(this string s)
+    {
+        return string.IsNullOrWhiteSpace(s);
+    }
+    /// <summary>
+    /// 判断字符串是否不为Null、空
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static bool NotNull(this string s)
+    {
+    return !string.IsNullOrWhiteSpace(s);
+    }
 }
