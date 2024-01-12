892ffd91fefee3903ab6a3b7d878c7b25208ec97
XS <37208063+qq790382720@users.noreply.github.com>
2024-01-12 11:19:07 +0800

    Update QueryResultKind.cs

--- YoutubeDownloader.Core/Resolving/QueryResultKind.cs
+++ YoutubeDownloader.Core/Resolving/QueryResultKind.cs
@@ -2,9 +2,9 @@
 
 public enum QueryResultKind
 {
-    Video,
-    Playlist,
-    Channel,
-    Search,
-    Aggregate
+    Video,      // Represents a result of type "Video".
+    Playlist,   // Represents a result of type "Playlist".
+    Channel,    // Represents a result of type "Channel".
+    Search,     // Represents a result of type "Search".
+    Aggregate   // Represents a result of type "Aggregate".
 }
